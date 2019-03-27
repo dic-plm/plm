01095693a2c23c59bc0c1a63d0ee01dabb62a476
syogarshi <49001108+syogarshi@users.noreply.github.com>
2019-03-27 13:24:32 -0500

    Create docker_cleanup.sh

---
+++ docker_cleanup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+docker rmi -f `docker images | awk '{if (NR!=1) {print $3}}'`
+docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+##for non empty docker Volumes
+##docker volume ls -q | xargs --no-run-if-empty docker volume rm -v
+echo Killing all Containers
+for i in $( docker ps -q ) ; do
+  docker kill $i
+done
+
+echo Removing all Containers
+for i in $( docker ps -aq ) ; do
+  docker rm $i
+done
+
+echo Removing all Images
+for i in $( docker images -q ) ; do
+  docker rmi -f $i
+done
+
+echo Removing all Networks
+for i in $( docker network ls -q ) ; do
+  docker network rm $i
+done
+
+echo Removing all Volumes
+for i in $( docker volume ls -q ) ; do
+  docker volume rm $i
+done
